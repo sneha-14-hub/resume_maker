c8fca3542aaf80b827ec062ddb3c40e3c8c0ab67
sneha_dubey <yourselflove9760@gmail.com>
2026-01-09 20:56:04 +0530

    Add Project files

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import React, {  } from "react";
+// import Builder from "./builder";
+import Hero from "../components/hero/Hero";
+
+const ResumeBuilder = () => {
+  return (
+    <>
+    <Hero />
+    </>
+  );
+};
+
+export default ResumeBuilder;
